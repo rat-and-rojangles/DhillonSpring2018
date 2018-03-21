2ee6836bd74b1ef9e13dab4eb00e1068fe35b89a
UNC Support <uncsupport@dhcp-v80-000340.wireless.unc.edu>
2018-03-20 22:14:32 -0400

    code auto formatter consistenc

---
+++ Assets/SCRIPTS/Objects/Refrigerator.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Refrigerator : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+
+	void Something (int a, int b) {
+	}
+
+	// Update is called once per frame
+	void Update () {
+	}
+}
